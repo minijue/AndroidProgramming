18496b8bb251820bfd9915d3e2f4abe6bf4f033d
minijue <jue952@126.com>
2016-09-29 16:16:33 +0800

    新建ToDoList项目

---
+++ ToDoList/app/src/main/java/wangjue/todolist/ToDoListActivity.java
@@ -0,0 +1,13 @@
+package wangjue.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToDoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
