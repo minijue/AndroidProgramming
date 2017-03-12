4f5f32f13499d9cf74150d2cdce7b9e009082202
王珏 <jue952@live.cn>
2017-03-12 21:40:22 +0800

    完成示例2.2

---
+++ CustomView/app/src/main/java/com/example/jue95/customview/CustomViewActivity.java
@@ -0,0 +1,17 @@
+package com.example.jue95.customview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Wangjue on 2017/3/12.
+ */
+
+public class CustomViewActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
