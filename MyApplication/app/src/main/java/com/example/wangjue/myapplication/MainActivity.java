127e46881bb2edd88eb020530331b06f254fad7a
王珏 <jue952@live.cn>
2017-03-17 17:45:56 +0800

    重建项目文件

---
+++ MyApplication/app/src/main/java/com/example/wangjue/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.wangjue.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    int[] images = new int[]{
+            R.drawable.java,
+            R.drawable.javaee,
+            R.drawable.swift,
+            R.drawable.ajax,
+            R.drawable.html,
+    };
+
+    int currentImg = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout main = (LinearLayout) findViewById(R.id.root);
+        final ImageView image = new ImageView(this);
+        main.addView(image);
+
+        image.setImageResource(images[0]);
+        image.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                image.setImageResource(images[++currentImg % images.length]);
+            }
+
+        });
+    }
+}
