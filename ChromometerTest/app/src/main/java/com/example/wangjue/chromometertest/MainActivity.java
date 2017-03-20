2701f4e90278094223fd6014130e4a74462391a6
王珏 <jue952@live.cn>
2017-03-20 15:45:19 +0800

    新建 ChronometerTest 项目

---
+++ ChromometerTest/app/src/main/java/com/example/wangjue/chromometertest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.wangjue.chromometertest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Chronometer;
+
+public class MainActivity extends AppCompatActivity {
+    Chronometer ch;
+    Button start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
