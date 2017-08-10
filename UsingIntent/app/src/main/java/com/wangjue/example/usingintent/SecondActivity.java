2e77d98d81eae9a46644cf25031a2156dac44044
王珏 <jue952@live.cn>
2017-08-10 17:04:29 +0800

    3.3 用Intent启动Activity

---
+++ UsingIntent/app/src/main/java/com/wangjue/example/usingintent/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.wangjue.example.usingintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by root on 17-8-10.
+ */
+
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
