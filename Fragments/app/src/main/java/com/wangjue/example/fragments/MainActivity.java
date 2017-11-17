9a98cec332b4f3154f4f25b879265758aa2ada9b
王珏 <jue952@live.cn>
2017-11-17 13:59:57 +0800

    动态添加Fragment

--- Fragments/app/src/main/java/com/wangjue/example/fragments/MainActivity.java
+++ Fragments/app/src/main/java/com/wangjue/example/fragments/MainActivity.java
@@ -1,13 +1,31 @@
 package com.wangjue.example.fragments;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        DisplayMetrics display = this.getResources().getDisplayMetrics();
+
+        int width = display.widthPixels;
+        int height = display.heightPixels;
+        if (width > height) {
+            Fragment1 fragment1 = new Fragment1();
+            fragmentTransaction.replace(android.R.id.content, fragment1);
+        } else {
+            Fragment2 fragment2 = new Fragment2();
+            fragmentTransaction.replace(android.R.id.content, fragment2);
+        }
+        fragmentTransaction.commit();
+        // setContentView(R.layout.activity_main);
     }
 }
