617938555c9bc39e6729f6401b0d217adbb3c3ea
王珏 <jue952@live.cn>
2017-11-17 14:35:09 +0800

    Fragment之间的通信

--- Fragments/app/src/main/java/com/wangjue/example/fragments/Fragment2.java
+++ Fragments/app/src/main/java/com/wangjue/example/fragments/Fragment2.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -17,6 +20,17 @@ public class Fragment2 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment2, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Button btnGetText = getActivity().findViewById(R.id.btnGetText);
+        btnGetText.setOnClickListener((v) -> {
+            TextView lbl = getActivity().findViewById(R.id.lblFragment1);
+            Toast.makeText(getActivity(), lbl.getText(), Toast.LENGTH_SHORT).show();
+        });
     }
 
 }
--- Fragments/app/src/main/java/com/wangjue/example/fragments/MainActivity.java
+++ Fragments/app/src/main/java/com/wangjue/example/fragments/MainActivity.java
@@ -1,17 +1,14 @@
 package com.wangjue.example.fragments;
 
 import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+/*
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         DisplayMetrics display = this.getResources().getDisplayMetrics();
@@ -26,6 +23,7 @@ public class MainActivity extends Activity {
             fragmentTransaction.replace(android.R.id.content, fragment2);
         }
         fragmentTransaction.commit();
-        // setContentView(R.layout.activity_main);
+        */
+        setContentView(R.layout.activity_main);
     }
 }
