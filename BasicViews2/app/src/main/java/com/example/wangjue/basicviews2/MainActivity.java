8e9fb9bc9798d9f3a9e99542c24cbd41a5d2c393
王珏 <jue952@live.cn>
2018-01-29 17:29:43 +0800

    创建项目 BasicViews2

---
+++ BasicViews2/app/src/main/java/com/example/wangjue/basicviews2/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.wangjue.basicviews2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class MainActivity extends Activity {
+
+    static int progress;
+    ProgressBar progressBar;
+
+    int progressStatus = 0;
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progress = 0;
+        progressBar = findViewById(R.id.progressbar);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (progressStatus < 10) {
+                    progressStatus = doSomeWork();
+                }
+
+                handler.post(() -> {
+                    progressBar.setVisibility(View.GONE);
+                });
+            }
+
+            private int doSomeWork() {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return ++progress;
+            }
+        }).start();
+    }
+}
