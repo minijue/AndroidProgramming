22c6214c6a8ece51fa457c290dd39dfb31701ef1
王珏 <jue952@live.cn>
2018-01-31 11:15:21 +0800

    修改项目 BasicView4，增加 DatePicker 示例

--- BasicView4/app/src/main/java/com/example/wangjue/basicview4/MainActivity.java
+++ BasicView4/app/src/main/java/com/example/wangjue/basicview4/MainActivity.java
@@ -4,13 +4,18 @@ import android.app.Activity;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends Activity {
     TimePicker timePicker;
+    DatePicker datePicker;
+    int hour, minute;
 
     Calendar cal = Calendar.getInstance();
 
@@ -22,20 +27,29 @@ public class MainActivity extends Activity {
         timePicker = findViewById(R.id.timePicker);
         timePicker.setIs24HourView(true);
 
+        datePicker = findViewById(R.id.datePicker);
+
         showTimeDialog();
     }
 
     private void showTimeDialog() {
         new TimePickerDialog(MainActivity.this, (timePicker, i, il) -> {
-            Toast.makeText(getBaseContext(), "Time selected: " + timePicker.getHour() + ":" + timePicker.getMinute(),
-                    Toast.LENGTH_SHORT).show();
+            hour = i;
+            minute = il;
+
+            SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm aa");
+            Date date = new Date();
+            String strDate = timeFormat.format(date);
+
+            Toast.makeText(getBaseContext(), "You have selected " + strDate, Toast.LENGTH_SHORT).show();
         }, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false).show();
         {
         }
     }
 
     public void onClick(View view) {
-        Toast.makeText(getBaseContext(), "Time selected: " + timePicker.getHour() + ":" + timePicker.getMinute(),
-                Toast.LENGTH_SHORT).show();
+//      Toast.makeText(getBaseContext(), "Time selected: " + timePicker.getHour() + ":" + timePicker.getMinute(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(getBaseContext(), "Date selected: " + datePicker.getYear() + "-" + (datePicker.getMonth() + 1) + "-" + datePicker.getDayOfMonth()
+                + "\n" + "Time selected: " + timePicker.getHour() + ":" + timePicker.getMinute(), Toast.LENGTH_SHORT).show();
     }
 }
