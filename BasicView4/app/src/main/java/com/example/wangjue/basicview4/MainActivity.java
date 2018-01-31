8111c0a2e64e96b2d9906d1d56ea324b9fa164a6
王珏 <jue952@live.cn>
2018-01-31 11:00:08 +0800

    修改项目 BasicView4，增加 TimePickerDialog 示例

--- BasicView4/app/src/main/java/com/example/wangjue/basicview4/MainActivity.java
+++ BasicView4/app/src/main/java/com/example/wangjue/basicview4/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.wangjue.basicview4;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class MainActivity extends Activity {
     TimePicker timePicker;
+
+    Calendar cal = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,17 @@ public class MainActivity extends Activity {
 
         timePicker = findViewById(R.id.timePicker);
         timePicker.setIs24HourView(true);
+
+        showTimeDialog();
+    }
+
+    private void showTimeDialog() {
+        new TimePickerDialog(MainActivity.this, (timePicker, i, il) -> {
+            Toast.makeText(getBaseContext(), "Time selected: " + timePicker.getHour() + ":" + timePicker.getMinute(),
+                    Toast.LENGTH_SHORT).show();
+        }, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false).show();
+        {
+        }
     }
 
     public void onClick(View view) {
