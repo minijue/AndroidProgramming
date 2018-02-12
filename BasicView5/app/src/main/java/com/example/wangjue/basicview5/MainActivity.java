d1beb41f92d0a40f41fb5d51d9d56d72fd10ca39
王珏 <jue952@live.cn>
2018-02-12 14:48:41 +0800

    创建项目 BasicView5，ListActivity

---
+++ BasicView5/app/src/main/java/com/example/wangjue/basicview5/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.wangjue.basicview5;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+    String[] presidents = {
+            "Dwight D. Eisenhower",
+            "John F. Kennedy",
+            "Lyndon B. Johnson",
+            "Richard Nixon",
+            "Gerald Ford",
+            "Jimmy Carter",
+            "Ronald Reagan",
+            "George H. W. Bush",
+            "Bill Clinton",
+            "George W. Bush",
+            "Barack Obama",
+            "Donald J. Trump"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, presidents));
+    }
+
+    public void onListItemClick(ListView parent, View v, int position, long id) {
+        Toast.makeText(this, "You have selected " + presidents[position], Toast.LENGTH_SHORT).show();
+    }
+}
