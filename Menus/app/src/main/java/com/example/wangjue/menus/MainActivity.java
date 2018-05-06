95249f7686f1fdd175a72a39fcd38630ff55dc74
王珏 <jue952@live.cn>
2018-05-06 16:58:46 +0800

    为浮动按钮增加上下文菜单示例

--- Menus/app/src/main/java/com/example/wangjue/menus/MainActivity.java
+++ Menus/app/src/main/java/com/example/wangjue/menus/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.wangjue.menus;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,13 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnCreateContextMenuListener(this);
     }
 
     private void createMenu(Menu menu) {
@@ -88,6 +82,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        return MenuChoice(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        createMenu(menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
         return MenuChoice(item);
     }
 }
