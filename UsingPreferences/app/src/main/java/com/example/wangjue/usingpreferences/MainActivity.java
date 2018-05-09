045dab799ed2d11e9d8040d0e62812954a1c6b8c
王珏 <jue952@live.cn>
2018-05-09 15:06:05 +0800

    Modify Project "usingpreferences", add displaying and modifying preference text funciton.

--- UsingPreferences/app/src/main/java/com/example/wangjue/usingpreferences/MainActivity.java
+++ UsingPreferences/app/src/main/java/com/example/wangjue/usingpreferences/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.wangjue.usingpreferences;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -19,9 +22,19 @@ public class MainActivity extends Activity {
     }
 
     public void onClickDisplay(View view) {
+        SharedPreferences appPrefs = getSharedPreferences("com.example.wangjue.usingpreferences_preferences", MODE_PRIVATE);
+        DisplayText(appPrefs.getString("editTextPref", ""));
+    }
 
+    private void DisplayText(String str) {
+        Toast.makeText(getBaseContext(), str, Toast.LENGTH_SHORT).show();
     }
 
     public void onClickModify(View view) {
+        SharedPreferences appPrefs = getSharedPreferences("com.example.wangjue.usingpreferences_preferences", MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = appPrefs.edit();
+        prefsEditor.putString("editTextPref", ((EditText) findViewById(R.id.editText)).getText().toString());
+        prefsEditor.commit();
+
     }
 }
