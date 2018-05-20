9535d65883ab09a84b2d7566face333affea262d
王珏 <jue952@live.cn>
2018-05-20 18:20:10 +0800

    1. 增加更新数据库记录功能 2. 增加删除数据库记录功能 3. 实现界面和数据库记录集的同步

--- Databases/app/src/main/java/com/example/wangjue/databases/MainActivity.java
+++ Databases/app/src/main/java/com/example/wangjue/databases/MainActivity.java
@@ -8,12 +8,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.TreeSet;
+
 public class MainActivity extends AppCompatActivity {
     private static int index = 1;
+    TreeSet<Integer> idset = new TreeSet<Integer>();
 
-    String name, email;
+    String name = "", email = "";
     EditText txtName, txtEmail;
-    Button btnPrev, btnNext;
+    Button btnPrev, btnNext, btnUpdate, btnDel;
     DBAdapter db;
 
     @Override
@@ -26,12 +29,21 @@ public class MainActivity extends AppCompatActivity {
 
         btnNext = findViewById(R.id.btnNext);
         btnPrev = findViewById(R.id.btnPrev);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDel = findViewById(R.id.btnDel);
 
         db = new DBAdapter(this);
         db.open();
         long id = db.insertContact("Wangjue", "jue952@live.cn");
+        idset.add((int) id);
         id = db.insertContact("Zhouli", "sunnyzlwj@hotmail.com");
+        idset.add((int) id);
         db.close();
+
+        btnPrev.setEnabled(false);
+        btnNext.setEnabled(false);
+        btnUpdate.setEnabled(false);
+        btnDel.setEnabled(false);
 
         db.open();
         /*Cursor c = db.getAllContacts();
@@ -41,44 +53,99 @@ public class MainActivity extends AppCompatActivity {
             } while (c.moveToNext());
         }
         */
-        displayContact();
+        displayContact(index);
 
         db.close();
     }
 
-    private void displayContact() {
-        Cursor c = db.getContact(index);
+    private void displayContact(/*Cursor c*/int i) {
+//        Toast.makeText(getBaseContext(), "id: " + c.getString(0) + "\n"
+//                + "Name: " + c.getString(1) + "\n"
+//                + "Email: " + c.getString(2), Toast.LENGTH_SHORT).show();
+        Cursor c = db.getContact(i);
         if (c.moveToFirst()) {
+            btnUpdate.setEnabled(true);
+
             name = c.getString(1);
             email = c.getString(2);
 
-            txtName.setText(name);
-            txtEmail.setText(email);
+            boolean hasLess = false;
+            boolean hasBigger = false;
+
+            for (Integer item : idset) {
+                if (item.longValue() > i) {
+                    hasBigger = true;
+                    break;
+                } else if (item.longValue() < i) {
+                    hasLess = true;
+                    break;
+                }
+            }
+
+            btnPrev.setEnabled(hasLess);
+            btnNext.setEnabled(hasBigger);
+            btnUpdate.setEnabled(true);
+            btnDel.setEnabled(true);
+        }
+        if (i == 0) {
+            name = "";
+            email = "";
+            btnPrev.setEnabled(false);
+            btnNext.setEnabled(false);
+            btnUpdate.setEnabled(false);
+            btnDel.setEnabled(false);
         }
 
-        btnPrev.setEnabled(index > 1);
-        btnNext.setEnabled(index < db.getContactsCount());
-    }
-
-    private void DisplayContract(Cursor c) {
-        Toast.makeText(getBaseContext(), "id: " + c.getString(0) + "\n"
-                + "Name: " + c.getString(1) + "\n"
-                + "Email: " + c.getString(2), Toast.LENGTH_SHORT).show();
+        txtName.setText(name);
+        txtEmail.setText(email);
     }
 
     public void onClick(View view) {
         db.open();
         switch (view.getId()) {
             case R.id.btnPrev:
-                if (index > 1) {
-                    --index;
-                    displayContact();
+                if (idset.contains(index)) {
+                    displayContact(--index);
                 }
                 break;
             case R.id.btnNext:
-                if (index < db.getContactsCount()) {
-                    ++index;
-                    displayContact();
+                if (idset.contains(index)) {
+                    displayContact(++index);
+                }
+                break;
+            case R.id.btnUpdate:
+                String newName = txtName.getText().toString();
+                String newEmail = txtEmail.getText().toString();
+                if (idset.contains(index) &&
+                        (!name.equals(newName) || !email.equals(newEmail))) {
+                    if (db.updateContact(index, newName, newEmail)) {
+                        Toast.makeText(this, "Update Successful.", Toast.LENGTH_SHORT).show();
+                        displayContact(index);
+                    } else {
+                        Toast.makeText(this, "Update failed.", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "数据没有改变，不需要更新！", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.btnDel:
+                if (idset.contains(index)) {
+                    if (db.deleteContact(index)) {
+                        idset.remove(index);
+                        Integer I = idset.higher(index);
+                        if (I != null) {
+                            index = I.intValue();
+                        } else {
+                            if (idset.size() > 0)
+                                index = idset.last().intValue();
+                            else
+                                index = 0;
+                        }
+                        displayContact(index);
+                        Toast.makeText(this, "Delete successful.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Delete failed.", Toast.LENGTH_SHORT).show();
+                    }
                 }
                 break;
         }
