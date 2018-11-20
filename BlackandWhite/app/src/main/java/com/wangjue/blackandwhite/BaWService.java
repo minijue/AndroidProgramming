ce8d4f785137b68fb6d3094e4d4c8aaee14a4649
王珏 <jue952@126.com>
2018-11-20 21:47:40 +0800

    a app for changing screen to black&white mode

---
+++ BlackandWhite/app/src/main/java/com/wangjue/blackandwhite/BaWService.java
@@ -0,0 +1,80 @@
+package com.wangjue.blackandwhite;
+
+import android.database.ContentObserver;
+import android.graphics.drawable.Icon;
+import android.os.Handler;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import android.util.Log;
+
+public class BaWService extends TileService {
+    private Icon icon;
+    private boolean toggleState = false;
+
+    private ContentObserver mBrightnessObserver = new ContentObserver(new Handler()) {
+        @Override
+        public void onChange(boolean selfChange) {
+            if (toggleState) {
+                taggleScreeen(toggleState);
+            }
+        }
+    };
+
+    private void taggleScreeen(boolean b) {
+        toggleState = b;
+        icon = Icon.createWithResource(getApplicationContext(), R.drawable.tv);
+        int active = b ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
+        getQsTile().setState(active);// 更改成非活跃状态
+
+        int value1 = b ? Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL : Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
+        int value2 = b ? 1 : 0;
+        Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, value1);
+//        if (b) {
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        Settings.Secure.putInt(getContentResolver(), "accessibility_display_daltonizer_enabled", value2);
+    }
+
+    @Override
+    public void onTileAdded() {
+    }
+
+    //当用户从快速设定栏中移除的时候调用
+    @Override
+    public void onTileRemoved() {
+    }
+
+
+    @Override
+    public void onClick() {
+        toggleState = !toggleState;
+        taggleScreeen(toggleState);
+
+        getQsTile().setIcon(icon);//设置图标
+        getQsTile().updateTile();//更新Tile
+    }
+
+    @Override
+    public void onStartListening() {
+
+    }
+
+    // 关闭下拉菜单的时候调用,当快速设置按钮并没有在编辑栏拖到设置栏中不会调用
+    // 在onTileRemoved移除之前也会调用移除
+    @Override
+    public void onStopListening() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        getApplicationContext().getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS), true,
+                mBrightnessObserver);
+    }
+}
